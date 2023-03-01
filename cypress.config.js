8bdf6edc2e7931e8b881f66e7ba962f377a1179b
yahya <yhyyzcgl@gmail.com>
2023-03-01 20:13:20 +0300

    01.03.23

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,19 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:"https://www.google.com",
-    projectId: "9btwvj"
+    baseUrl : "https://www.google.com",
+
+    projectId: "kf9cf4",
+    
+    "video": false,
+    //testlerin videoları kaydetmiyor
+    //"video": true: testlerin videolarını kaydeder.
+    //default olarak "video": true dur.
+
+    //"retries": 2
+    //Test FAIL olduğunda onu kaç kere daha çalıştırması gerektiğini belirtiyoruz.
+    // "retries": 2 => 1 kere çalıştırıp hata aldığında; 2 defa daha dene
+ 
+    
   },
 });
